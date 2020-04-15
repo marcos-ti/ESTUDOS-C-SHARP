d70521252cc81f6fbe249068976426510c35f0ef
Marcos Faria <saoroque@hotmail.com>
2020-04-15 15:07:47 -0300

    EXERCICIOS PROPOSTOS DIA 15-04-2020

---
+++ EXERCICIO_PROPOSTO_04/ConsoleApp1EXERCICIO_PROPOSTO_04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1EXERCICIO_PROPOSTO_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu número de funcionário: ");
+            int numFun = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite suas horas trabalhadas: ");
+            int horasTrab = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor que recebe por hora trabalhada: ");
+            double valHora = double.Parse(Console.ReadLine());
+
+            double soma = horasTrab * valHora;
+
+            Console.WriteLine("Seus resultados são: " + "Funcionário Número: " + numFun);
+            Console.WriteLine("Salário = R$ " + soma.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ EXERCICIO_PROPOSTO_05/EXERCICIO_PROPOSTO_05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace EXERCICIO_PROPOSTO_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Digite o código, o número e o valor unitário das peças (1): ");
+
+            int cod1, cod2, num2, num1;
+            double val1, val2, valTotal;
+
+            string[] valores = Console.ReadLine().Split(' ');//DECLAREI A STRING VALORES
+            cod1 = int.Parse(valores[0]);
+            num1 = int.Parse(valores[1]);
+            val1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            ///////
+
+            Console.WriteLine("Digite o código, o número e o valor unitário das peças (2): ");
+
+            valores = Console.ReadLine().Split(' ');//CHAMEI A STRING JÁ DECLARADA
+            cod2 = int.Parse(valores[0]);
+            num2 = int.Parse(valores[1]);
+            val2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            valTotal = (val1 * num1) + (val2 * num2);
+
+            Console.WriteLine("O valor a ser pago é: R$" + valTotal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+    }
+}
